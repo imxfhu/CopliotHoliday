607d4f0e3fad0d4ac818e00481e47506b2400c20
IriesHu <imxfhu@163.com>
2023-07-21 19:22:49 +0800

    fix

--- src/main/java/com/example/copliotholiday/service/HolidayServiceImp.java
+++ src/main/java/com/example/copliotholiday/service/HolidayServiceImp.java
@@ -2,6 +2,7 @@ package com.example.copliotholiday.service;
 import com.example.copliotholiday.entity.HolidayEntity;
 import com.example.copliotholiday.entity.HolidayEntityList;
 import com.example.copliotholiday.utils.CSVUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -50,8 +51,42 @@ public class HolidayServiceImp implements HolidayService {
 
     @Override
     public HolidayEntity getNextHoliday(String holidayCountry) {
-        //TODO
-        return null;
+        HolidayEntityList holidayList = CSVUtils.getHolidayEntityList();
+        if(holidayList == null || holidayList.getHolidayList() == null || holidayList.getHolidayList().size() == 0){
+            return null;
+        }
+        Calendar now = Calendar.getInstance();
+        int currentYear = now.get(Calendar.YEAR);
+        int currentMonth = now.get(Calendar.MONTH) + 1;
+        int currentDay = now.get(Calendar.DAY_OF_MONTH);
+        int currentDate = currentYear*10000 + currentMonth*100 + currentDay;
+        int nextDate = Integer.MAX_VALUE;
+        int holidayDate = 0;
+        HolidayEntity holidayEntity = null;
+        ArrayList<HolidayEntity> currentHolidayList = holidayList.getHolidayList();
+        for(int i=0; i<currentHolidayList.size(); i++){
+            HolidayEntity holiday = currentHolidayList.get(i);
+            if(holiday.getHolidayCountry().equals(holidayCountry)){
+                holidayDate = getHolidayDate(holiday.getHolidayDate());
+                if(holidayDate>currentDate && holidayDate<nextDate){
+                    nextDate = holidayDate;
+                    holidayEntity = holiday;
+                }
+            }
+        }
+        return holidayEntity;
+    }
+
+    private int getHolidayDate(String holidayDate) {
+        if(StringUtils.isBlank(holidayDate)) {
+            return 0;
+        }
+        String[] dateStrs = holidayDate.split("-");
+        if(dateStrs == null || dateStrs.length != 3){
+            return 0;
+        }
+        int holidayDateInt = Integer.parseInt(dateStrs[0])*10000 + Integer.parseInt(dateStrs[1])*100 + Integer.parseInt(dateStrs[2]);
+        return holidayDateInt;
     }
 
     @Override
